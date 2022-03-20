c6bee6681552abf0c59ce65b90be0a2607bf25d0
zare926 <ilove.boc926@gmail.com>
2022-03-21 08:29:01 +0900

    controller setting

--- Api/src/config/routes.rb
+++ Api/src/config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
       resources :test, only: %i[index]
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
